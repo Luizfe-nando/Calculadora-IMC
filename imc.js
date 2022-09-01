850792637fae33d5c19d9b22263e0ba2758dc825
Luizfe-nando <luizfernandomoreirapinto@gmail.com>
2022-09-01 14:02:33 -0300

    calculadora imc

---
+++ imc.js
@@ -0,0 +1,8 @@
+//Vamos criar um programa para calcular o IMC //
+//para isso o calculo é altura x altura / pelo peso//
+
+let peso = 76;
+let altura = 1.79;
+let i = peso / (altura ** 2);
+
+console.log(`Se minha altura é ${altura} mtrs e meu peso, ${peso}, meu IMC é, ${i}`);
